667b9b0ed8d9f436818ebf12ccba29d90da2016e
Milosz Jablonski <milosz.jablonski@focal.systems>
2024-03-23 21:02:59 +0100

    chore: test

--- load_test.py
+++ load_test.py
@@ -2,6 +2,7 @@ from locust import between
 
 from mongo_user import MongoUser, mongodb_task
 from settings import DEFAULTS
+from bson import ObjectId
 
 import pymongo
 import random
@@ -104,55 +105,29 @@ class MongoSampleUser(MongoUser):
         }
         return document
 
-    # @mongodb_task(weight=int(DEFAULTS['AGG_PIPE_WEIGHT']))
-    # def run_aggregation_pipeline(self):
-    #     """
-    #     Run an aggregation pipeline on a secondary node
-    #     """
-    #     # count number of inhabitants per city
-    #     group_by = {
-    #         '$group': {
-    #             '_id': '$city',
-    #             'total_inhabitants': {'$sum': 1}
-    #         }
-    #     }
-    #
-    #     # rename the _id to city
-    #     set_columns = {'$set': {'city': '$_id'}}
-    #     unset_columns = {'$unset': ['_id']}
-    #
-    #     # sort by the number of inhabitants desc
-    #     order_by = {'$sort': {'total_inhabitants': pymongo.DESCENDING}}
-    #
-    #     pipeline = [group_by, set_columns, unset_columns, order_by]
-    #
-    #     # make sure we fetch everything by explicitly casting to list
-    #     # use self.collection instead of self.collection_secondary to run the pipeline on the primary
-    #     return list(self.collection_secondary.aggregate(pipeline))
-
     def on_start(self):
         """
         Executed every time a new test is started - place init code here
         """
         # prepare the collection
-        index1 = pymongo.IndexModel([('first_name', pymongo.ASCENDING), ("last_name", pymongo.DESCENDING)],
-                                    name="idx_first_last")
-        self.collection, self.collection_secondary = self.ensure_collection(DEFAULTS['COLLECTION_NAME'], [index1])
+        # WITHOUT INDEXES
+        # index1 = pymongo.IndexModel([('first_name', pymongo.ASCENDING), ("last_name", pymongo.DESCENDING)],
+        #                             name="idx_first_last")
+
+        self.collection, self.collection_secondary = self.ensure_collection(DEFAULTS['COLLECTION_NAME'], [])
         self.name_cache = []
 
     @mongodb_task(weight=int(DEFAULTS['INSERT_WEIGHT']))
     def insert_single_document(self):
         document = self.generate_new_document()
 
-        # cache the first_name, last_name tuple for queries
-        cached_names = (document['first_name'], document['last_name'])
+        inserted_result = self.collection.insert_one(document)
+
         if len(self.name_cache) < NAMES_TO_CACHE:
-            self.name_cache.append(cached_names)
+            self.name_cache.append(inserted_result.inserted_id)
         else:
             if random.randint(0, 9) == 0:
-                self.name_cache[random.randint(0, len(self.name_cache) - 1)] = cached_names
-
-        self.collection.insert_one(document)
+                self.name_cache[random.randint(0, len(self.name_cache) - 1)] = inserted_result.inserted_id
 
     @mongodb_task(weight=int(DEFAULTS['FIND_WEIGHT']))
     def find_document(self):
@@ -162,10 +137,12 @@ class MongoSampleUser(MongoUser):
 
         # find a random document using an index
         cached_names = random.choice(self.name_cache)
-        self.collection.find_one({'first_name': cached_names[0], 'last_name': cached_names[1]})
+        result = self.collection.find_one({'_id': ObjectId(cached_names)})
+        print(f"FIND RESULT: {result}")
 
-    @mongodb_task(weight=int(DEFAULTS['BULK_INSERT_WEIGHT']), batch_size=int(DEFAULTS['DOCS_PER_BATCH']))
-    def insert_documents_bulk(self):
-        self.collection.insert_many(
-            [self.generate_new_document() for _ in
-             range(int(DEFAULTS['DOCS_PER_BATCH']))])
+    # @mongodb_task(weight=int(DEFAULTS['BULK_INSERT_WEIGHT']), batch_size=int(DEFAULTS['DOCS_PER_BATCH']))
+    # def insert_documents_bulk(self):
+    #     AVOID_INSERT_MANY SCENARIO
+    #     self.collection.insert_many(
+    #         [self.generate_new_document() for _ in
+    #          range(int(DEFAULTS['DOCS_PER_BATCH']))])
